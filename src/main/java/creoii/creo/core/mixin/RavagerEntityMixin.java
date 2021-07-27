cc1fe239f236f56c1110db4109b9677d4c2cdc80
creoii <33011337+creoii@users.noreply.github.com>
2021-07-27 00:38:26 -0400

    fix mixin descriptor

--- src/main/java/creoii/creo/core/mixin/RavagerEntityMixin.java
+++ src/main/java/creoii/creo/core/mixin/RavagerEntityMixin.java
@@ -4,6 +4,7 @@ import creoii.creo.core.util.Tags;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.RavagerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ModifiableWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RavagerEntity.class)
 public class RavagerEntityMixin {
-    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ModifiableWorld;breakBlock(Lnet/minecraft/util/math/BlockPos;ZLnet/minecraft/entity/Entity)Z"))
-    private void tickMovement(BlockPos pos, boolean drop, Entity breakingEntity) {
+    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ModifiableWorld;breakBlock(Lnet/minecraft/util/math/BlockPos;ZLnet/minecraft/entity/Entity;)Z"))
+    private boolean tickMovement(ModifiableWorld modifiableWorld, BlockPos pos, boolean drop, Entity breakingEntity) {
         World world = breakingEntity.getEntityWorld();
         if (world.getBlockState(pos).isIn(Tags.Blocks.RAVAGER_BREAKABLE)) {
-            world.breakBlock(pos, true, breakingEntity);
-        }
+            return world.breakBlock(pos, drop, breakingEntity);
+        }  else return false;
     }
 }
