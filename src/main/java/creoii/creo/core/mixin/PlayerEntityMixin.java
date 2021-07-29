71141cd13215a55bbc0a9cafe048223f947f4a3c
creoii <33011337+creoii@users.noreply.github.com>
2021-07-29 19:13:58 -0400

    player immune blocks

--- src/main/java/creoii/creo/core/mixin/PlayerEntityMixin.java
+++ src/main/java/creoii/creo/core/mixin/PlayerEntityMixin.java
@@ -1,8 +1,12 @@
 package creoii.creo.core.mixin;
 
 import creoii.creo.core.registry.AttributeRegistry;
+import creoii.creo.core.util.BlockTags;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,5 +17,13 @@ public class PlayerEntityMixin {
     @Inject(method = "createPlayerAttributes", at = @At("RETURN"))
     private static void creo$createNewAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
         cir.getReturnValue().add(AttributeRegistry.GENERIC_NATURAL_REGENERATION, 1.0F);
+    }
+
+    /**
+     * TODO: You should be able to hit the block
+     */
+    @Inject(method = "isBlockBreakingRestricted", at = @At("HEAD"), cancellable = true)
+    private void creo$playerImmuneBlocks(World world, BlockPos pos, GameMode gameMode, CallbackInfoReturnable<Boolean> cir) {
+        if (world.getBlockState(pos).isIn(BlockTags.PLAYER_IMMUNE)) cir.setReturnValue(true);
     }
 }
--- src/main/java/creoii/creo/core/util/BlockTags.java
+++ src/main/java/creoii/creo/core/util/BlockTags.java
@@ -7,7 +7,8 @@ import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class BlockTags {
-    public static final Tag<Block> BLOCKS_VEX = TagRegistry.block(new Identifier(Creo.MOD_ID, "blocks_vex"));
-    public static final Tag<Block> RAVAGER_BREAKABLE = TagRegistry.block(new Identifier(Creo.MOD_ID, "ravager_breakable"));
     public static final Tag<Block> AFFECTED_BY_GRAVITY = TagRegistry.block(new Identifier(Creo.MOD_ID, "affected_by_gravity"));
+    public static final Tag<Block> BLOCKS_VEX = TagRegistry.block(new Identifier(Creo.MOD_ID, "blocks_vex"));
+    public static final Tag<Block> PLAYER_IMMUNE = TagRegistry.block(new Identifier(Creo.MOD_ID, "player_immune"));
+    public static final Tag<Block> RAVAGER_BREAKABLE = TagRegistry.block(new Identifier(Creo.MOD_ID, "ravager_breakable"));
 }
