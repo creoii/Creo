49aca175715e1398c752aa3626dd50eb1aa9bda7
creoii <33011337+creoii@users.noreply.github.com>
2021-07-29 20:40:11 -0400

    another attempt at hopper immunes

--- src/main/java/creoii/creo/core/mixin/HopperBlockEntityMixin.java
+++ src/main/java/creoii/creo/core/mixin/HopperBlockEntityMixin.java
@@ -2,18 +2,28 @@ package creoii.creo.core.mixin;
 
 import creoii.creo.core.util.ItemTags;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.Hopper;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(HopperBlockEntity.class)
 public class HopperBlockEntityMixin {
+    @Inject(method = "getInputItemEntities", at = @At("RETURN"))
+    private static void creo$hopperImmunes(World world, Hopper hopper, CallbackInfoReturnable<List<ItemEntity>> cir) {
+        cir.getReturnValue().stream().filter((entity) -> entity.getStack().isIn(ItemTags.HOPPER_IMMUNE)).toList();
+    }
+
     @Inject(method = "onEntityCollided", at = @At("HEAD"), cancellable = true)
     private static void creo$hopperImmunes(World world, BlockPos pos, BlockState state, Entity entity, HopperBlockEntity blockEntity, CallbackInfo ci) {
         if (entity instanceof ItemEntity) {
